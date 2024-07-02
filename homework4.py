95491cab704ed45daea2c880b85064d565b0c044
Vdavchansky <51847880+Vdavchansky@users.noreply.github.com>
2024-07-02 07:55:05 +0600

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+my_string = input("Напишите что-нибудь: ")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
