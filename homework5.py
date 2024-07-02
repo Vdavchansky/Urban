a517831e22a604b7e3745912a7ffb80c79d04a7d
Vdavchansky <51847880+Vdavchansky@users.noreply.github.com>
2024-07-03 02:28:34 +0600

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,6 +1,6 @@
 immutable_var = 1,2,3,"Alex",True,1.0
 print(immutable_var)
-# immutable_var = 1,2,3,4,5,6,7,8
+# immutable_var[0] = 200 Нельзя изменить, потому что элементы кортежа хранят адресса в памяти на объект, а не сам объект.
 mutable_list = [0,1,2,3,"четыре",5,True,7.0]
 print(mutable_list)
 mutable_list.remove(0)
