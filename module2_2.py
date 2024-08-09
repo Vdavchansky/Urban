b01cc7ba97891682556abd41db374d0a4f36cc86
Vdavchansky <51847880+Vdavchansky@users.noreply.github.com>
2024-08-09 12:31:27 +0600

    Add files via upload

--- module2_2.py
+++ module2_2.py
@@ -1,9 +1,9 @@
 x = int(input("Введите первое число: "))
 y = int(input("Введите второе число: "))
 z = int(input("Введите третье число: "))
-if x==y==z:
+if x == y == z:
     print(3)
-elif x==y or x==z or y==z:
+elif x == y or x == z or y == z:
     print(2)
-elif x!=y!=z:
+elif x != y != z:
     print(0)
